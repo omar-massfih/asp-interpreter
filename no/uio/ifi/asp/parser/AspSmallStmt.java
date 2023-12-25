32fba99f9fb10cc351e05a227bdaa00b53317827
Omar Massfih <omar-massfih@hotmail.com>
2023-12-25 23:25:07 +0100

    Implemented parse and prettyPrint in AspSmallStmtList.java

---
+++ no/uio/ifi/asp/parser/AspSmallStmt.java
@@ -0,0 +1,17 @@
+package no.uio.ifi.asp.parser;
+
+import no.uio.ifi.asp.scanner.Scanner;
+
+public class AspSmallStmt {
+
+    public static AspSmallStmt parse(Scanner s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parse'");
+    }
+
+    public void prettyPrint() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'prettyPrint'");
+    }
+
+}
--- no/uio/ifi/asp/parser/AspSmallStmtList.java
+++ no/uio/ifi/asp/parser/AspSmallStmtList.java
@@ -1,12 +1,57 @@
 package no.uio.ifi.asp.parser;
 
+import java.util.ArrayList;
+import static no.uio.ifi.asp.scanner.TokenKind.newLineToken;
+import static no.uio.ifi.asp.scanner.TokenKind.semicolonToken;
+import no.uio.ifi.asp.runtime.RuntimeReturnValue;
+import no.uio.ifi.asp.runtime.RuntimeScope;
+import no.uio.ifi.asp.runtime.RuntimeValue;
 import no.uio.ifi.asp.scanner.Scanner;
 
-public class AspSmallStmtList {
+public class AspSmallStmtList extends AspStmt{
+    ArrayList<AspSmallStmt> aspSmallStatetments = new ArrayList<>();
 
-    public static AspStmt parse(Scanner s) {
+    AspSmallStmtList(int lineNumber) {
+        super(lineNumber);
+    }
+
+    public static AspSmallStmtList parse(Scanner s) {
+        enterParser("small stmt list");
+        AspSmallStmtList aspSmallStmtList = new AspSmallStmtList(s.curLineNum());
+        aspSmallStmtList.aspSmallStatetments.add(AspSmallStmt.parse(s));
+        
+        while (s.curToken().kind == semicolonToken) {
+            skip(s, semicolonToken);
+
+            if (s.curToken().kind == newLineToken) {
+                break;
+            }
+
+            aspSmallStmtList.aspSmallStatetments.add(AspSmallStmt.parse(s));
+        }
+
+        skip(s, newLineToken);
+        leaveParser("small stmt list");
+        return aspSmallStmtList;
+    }
+
+    @Override
+    void prettyPrint() {
+        for (int i = 0; i < aspSmallStatetments.size(); i++) {
+            aspSmallStatetments.get(i).prettyPrint();
+
+            if (i < aspSmallStatetments.size() - 1) {
+                prettyWrite("; ");
+            }
+        }
+        
+        prettyWriteLn();
+    }
+
+    @Override
+    RuntimeValue eval(RuntimeScope curScope) throws RuntimeReturnValue {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parse'");
+        throw new UnsupportedOperationException("Unimplemented method 'eval'");
     }
 
 }
