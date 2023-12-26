a0407e98edca230649e519d001a255542ff564db
Omar Massfih <omar-massfih@hotmail.com>
2023-12-26 17:57:36 +0100

    Implemented parse and prettyPrint in AspExprStmt.java

--- no/uio/ifi/asp/parser/AspExprStmt.java
+++ no/uio/ifi/asp/parser/AspExprStmt.java
@@ -1,12 +1,35 @@
 package no.uio.ifi.asp.parser;
 
+import no.uio.ifi.asp.runtime.RuntimeReturnValue;
+import no.uio.ifi.asp.runtime.RuntimeScope;
+import no.uio.ifi.asp.runtime.RuntimeValue;
 import no.uio.ifi.asp.scanner.Scanner;
 
-public class AspExprStmt {
+public class AspExprStmt extends AspSmallStmt{
+    AspExpr aspExpr;
 
-    public static AspSmallStmt parse(Scanner s) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parse'");
+    public AspExprStmt(int n) {
+        super(n);
     }
 
+    public static AspExprStmt parse(Scanner s) {
+        enterParser("expr stmt");
+        AspExprStmt aspExprStmt = new AspExprStmt(s.curLineNum());
+
+        aspExprStmt.aspExpr = AspExpr.parse(s);
+
+        leaveParser("expr stmt");
+        return aspExprStmt;
+    }
+
+    @Override
+    public void prettyPrint() {
+        aspExpr.prettyPrint();
+    }
+
+    @Override
+    RuntimeValue eval(RuntimeScope curScope) throws RuntimeReturnValue {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
