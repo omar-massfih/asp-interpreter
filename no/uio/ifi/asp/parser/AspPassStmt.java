c1ca783bbfeaf83bf2ba6ef1cdc1a3a784a2ecf7
Omar Massfih <omar-massfih@hotmail.com>
2023-12-26 17:56:09 +0100

    Implemented parse in AspSmallStmt.java

---
+++ no/uio/ifi/asp/parser/AspPassStmt.java
@@ -0,0 +1,12 @@
+package no.uio.ifi.asp.parser;
+
+import no.uio.ifi.asp.scanner.Scanner;
+
+public class AspPassStmt {
+
+    public static AspSmallStmt parse(Scanner s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parse'");
+    }
+
+}
--- no/uio/ifi/asp/parser/AspSmallStmt.java
+++ no/uio/ifi/asp/parser/AspSmallStmt.java
@@ -1,17 +1,35 @@
 package no.uio.ifi.asp.parser;
 
+import no.uio.ifi.asp.runtime.RuntimeReturnValue;
+import no.uio.ifi.asp.runtime.RuntimeScope;
+import no.uio.ifi.asp.runtime.RuntimeValue;
 import no.uio.ifi.asp.scanner.Scanner;
+import static no.uio.ifi.asp.scanner.TokenKind.*;
 
-public class AspSmallStmt {
+public class AspSmallStmt extends AspSyntax{
+
+    AspSmallStmt(int lineNumber) {
+        super(lineNumber);
+    }
 
     public static AspSmallStmt parse(Scanner s) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parse'");
-    }
+        enterParser("small stmt");
+        AspSmallStmt aspSmallStmt = null;
 
-    public void prettyPrint() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'prettyPrint'");
-    }
 
+        if (s.anyEqualToken()){
+            aspSmallStmt = AspAssignmentStmt.parse(s);
+        } else if (s.curToken().kind == passToken) {
+            aspSmallStmt = AspPassStmt.parse(s);
+        } else if (s.curToken().kind == returnToken) {
+            aspSmallStmt = AspReturnStmt.parse(s);
+        } else if (s.curToken().kind == globalToken) {
+            aspSmallStmt = AspGlobalStmt.parse(s);
+        } else {
+            aspSmallStmt = AspExprStmt.parse(s);
+        }
+        
+        leaveParser("small stmt");
+        return aspSmallStmt;
+    }
 }
